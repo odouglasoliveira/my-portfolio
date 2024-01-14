da04a52b15a20705f166b59bb5686716adabf57b
Douglas Oliveira <doglasolivera13@gmail.com>
2024-01-14 15:16:39 -0300

    substituição de styled components para tailwind css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,12 @@
-import { StyledFooter } from "../styles/styles"
 import Linkedin from "../assets/iconmonstr-linkedin-3.svg"
 import Github from "../assets/iconmonstr-github-1.svg"
 import Card from "./Card"
 
 export default function Footer() {
   return (
-    <>
-    <StyledFooter>
+    <footer>
       <Card link="https://www.linkedin.com/in/douglasoliveiradev/" image={Linkedin} />
       <Card link="https://github.com/odouglasoliveira" image={Github} />
-    </StyledFooter>
-    </>
+    </footer>
   )
 }
