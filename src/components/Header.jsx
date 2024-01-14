5e259a48eedf9f6d2dbdc84814c98494f4c377e0
Douglas Oliveira <doglasolivera13@gmail.com>
2024-01-14 16:32:44 -0300

    alteração das cores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="flex align-middle justify-between p-4 px-12">
       <section className="flex-col justify-center align-middle min-h-full">
         <p className="self-center text-2xl font-semibold">Douglas Oliveira</p>
-        <p className="self-center text-sm font-semibold text-sky-800">Desenvolvedor Fullstack</p>
+        <p className="self-center text-sm font-semibold text-gray-700">Desenvolvedor Fullstack</p>
       </section>
       <section className="flex align-middle justify-center gap-4">
         <a className="self-center font-semibold" href="https://www.linkedin.com/in/douglasoliveiradev/">Linkedin</a>
