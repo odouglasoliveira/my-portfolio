59c91d9aeb3bbcacd98cd2b3dd0b3198c3842d42
Douglas Oliveira <doglasolivera13@gmail.com>
2024-01-15 17:24:23 -0300

    flex-wrap no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="flex align-middle justify-between p-4 px-12">
+    <header className="flex align-middle justify-between p-4 px-12 flex-wrap">
       <section className="flex-col justify-center align-middle min-h-full">
         <p className="self-center text-2xl font-semibold">Douglas Oliveira</p>
         <p className="self-center text-sm font-semibold text-gray-700">Desenvolvedor Fullstack</p>
       </section>
-      <section className="flex align-middle justify-center gap-4">
+      <section className="flex align-middle justify-center gap-4 flex-wrap">
         <a className="self-center font-semibold link" target="_blank" href="https://www.linkedin.com/in/douglasoliveiradev/" rel="noreferrer">Linkedin</a>
         <a className="self-center font-semibold link" target="_blank" href="https://github.com/odouglasoliveira" rel="noreferrer">Github</a>
         <a className="self-center font-semibold link" target="_blank" href="mailto:odouglasoliveira.dev@gmail.com" rel="noreferrer">Email</a>
