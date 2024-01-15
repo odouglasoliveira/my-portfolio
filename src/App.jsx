c0eeb5a422b1f3b595fc2f857b4b7e092ea8dc55
Douglas Oliveira <doglasolivera13@gmail.com>
2024-01-15 18:33:04 -0300

    criação da seção sobre

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import './App.css'
 import BoxCards from './components/BoxCards'
+import AboutMe from './components/AboutMe'
 
 function App() {
   return (
     <>
       <Header />
+      <AboutMe />
       <BoxCards />
     </>
   )
--- src/components/BoxCards.jsx
+++ src/components/BoxCards.jsx
@@ -4,8 +4,11 @@ import trybetunesImage from '../assets/trybetunes.png'
 export default function BoxCards() {
   return (
     <>
-      <section className='flex justify-evenly'>
-        <Card title="Trybetunes" description="Projeto frontend simulando uma aplicação de músicas. Onde é possível buscar por artistas, acessar albuns, ouvir prévias e favoritar músicas." image={trybetunesImage} repo_url="https://github.com/odouglasoliveira/trybetunes" deploy_url="https://trybetunes-zeta-one.vercel.app/" />
+      <section>
+        <h3 className='text-center font-bold font-sans text-2xl'>Projetos</h3>
+        <section className='flex justify-evenly flex-wrap px-12 gap-12'>
+          <Card title="Trybetunes" description="Projeto frontend simulando uma aplicação de músicas. Onde é possível buscar por artistas, acessar albuns, ouvir prévias e favoritar músicas." image={trybetunesImage} repo_url="https://github.com/odouglasoliveira/trybetunes" deploy_url="https://trybetunes-zeta-one.vercel.app/" />
+        </section>
       </section>
     </>
   )
