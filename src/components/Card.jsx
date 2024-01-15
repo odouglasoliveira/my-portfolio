eccfe56753b8f7aea0a9e6b2e2097b9152175a83
Douglas Oliveira <doglasolivera13@gmail.com>
2024-01-15 18:03:43 -0300

    criação do componente card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
 import propTypes from 'prop-types'
 
-export default function Card({title, image, description, url}) {
+export default function Card({title, image, description, deploy_url, repo_url}) {
   return (
     <>
-      <section className='shadow-xl w-fit p-8 rounded-xl h-60'>
-        <img src={image} alt={`Imagem do projeto ${image}`} />
-        <p>{title}</p>
-        <p>{description}</p>
-        <a href={url}></a>
+      <section className='shadow-xl w-2/6 p-8 rounded-xl'>
+        <img className='w-100 rounded-md' src={image} alt={`Imagem do projeto ${title}`} />
+        <p className='font-bold py-4'>{title}</p>
+        <p className='pb-4'>{description}</p>
+        <section className='flex justify-between flex-wrap'>
+          <a className='bg-blue-600 hover:bg-blue-800 duration-200 text-white py-2 px-4 rounded-md' target='_blank' href={deploy_url} rel="noreferrer">Deploy</a>
+          <a className='bg-blue-600 hover:bg-blue-800 duration-200 text-white py-2 px-4 rounded-md' target='_blank' href={repo_url} rel="noreferrer">Repositório</a>
+        </section>
       </section>
     </>
   )
@@ -17,5 +20,6 @@ Card.propTypes = {
   title: propTypes.string.isRequired,
   image: propTypes.string.isRequired,
   description: propTypes.string.isRequired,
-  url: propTypes.string.isRequired,
+  deploy_url: propTypes.string.isRequired,
+  repo_url: propTypes.string.isRequired,
 }
