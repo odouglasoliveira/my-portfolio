9575b9754298d07c5cbd00ab04c65174ced9b9f7
Douglas Oliveira <doglasolivera13@gmail.com>
2024-05-22 15:18:02 -0300

    add new footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 py-4 text-center">
+      <p>&copy; 2024 Douglas Oliveira. Todos os direitos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
